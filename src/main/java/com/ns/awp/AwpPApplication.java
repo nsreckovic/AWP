4ed79f3af9ec93602d5b5dda7e9fb0ab39de86ef
Nikola <sreckonikola@gmail.com>
2020-12-14 23:18:39 +0100

    Initial commit

---
+++ src/main/java/com/ns/awp/AwpPApplication.java
@@ -0,0 +1,13 @@
+package com.ns.awp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwpPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwpPApplication.class, args);
+    }
+
+}
